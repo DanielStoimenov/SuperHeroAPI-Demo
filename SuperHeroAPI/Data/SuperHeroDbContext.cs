fa621e12ad4857b95c45e775448f47c683f62c24
Daniel Stoimenov <daniel.stoimenov@itsoft.bg>
2022-03-23 12:00:23 +0200

    Add project files.

---
+++ SuperHeroAPI/Data/SuperHeroDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class SuperHeroDbContext : DbContext
+    {
+        public SuperHeroDbContext(DbContextOptions<SuperHeroDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
