4f01c9b6da040c73f12bc89d376a37cc9ec80d01
Daniel Stoimenov <daniel.stoimenov@itsoft.bg>
2022-03-23 13:21:12 +0200

    refactoring

--- SuperHeroAPI/Data/Entities/SuperHero.cs
+++ SuperHeroAPI/Data/Entities/SuperHero.cs
@@ -1,9 +1,14 @@
-﻿namespace SuperHeroAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperHeroAPI
 {
     public class SuperHero
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(10, ErrorMessage = "The name is too long")]
         public string Name { get; set; } = string.Empty;
 
         public string FirstName { get; set; } = string.Empty;
