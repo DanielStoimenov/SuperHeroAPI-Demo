09595a524a1cdafbe6ea3d592e462fcd64b9d905
Daniel Stoimenov <daniel.stoimenov@itsoft.bg>
2022-05-18 16:13:01 +0300

    Add Data Constants

---
+++ SuperHeroAPI/Data/DataConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperHeroAPI.Data
+{
+    public class DataConstants
+    {
+        public const int NameMaxLength = 25;
+        public const int FirstNameMaxLength = 20;
+        public const int LastNameMaxLength = 20;
+        public const int PlaceMaxLength = 30;
+    }
+}
